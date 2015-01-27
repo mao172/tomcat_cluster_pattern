47a1c3c59dacf9582dbcc54262eb04f1d7e09e96
takasix <maruoka.takashi@tis.co.jp>
2015-01-27 04:02:19 +0000

    Fixed a exsample of if not the primary db that conditions had been reversed

--- site-cookbooks/postgresql_part/spec/recipes/deploy_spec.rb
+++ site-cookbooks/postgresql_part/spec/recipes/deploy_spec.rb
@@ -6,10 +6,9 @@ describe 'postgresql_part::deploy' do
 
   myself_hostname = 'myself'
   partner_hostname = 'partner'
+  app_name = 'app'
 
   describe 'primary db is this node' do
-    app_name = 'app'
-
     before do
       chef_run.node.set['cloudconductor']['applications'] = {
         app_name => {
@@ -19,7 +18,7 @@ describe 'postgresql_part::deploy' do
       chef_run.converge(described_recipe)
 
       response = format(['[{"Node":"%s","Address":"172.17.0.1","ServiceID":"db",',
-                             '"ServiceName":"db","ServiceTags":["primary"],"ServicePort":5432}]'].join, myself_hostname)
+                         '"ServiceName":"db","ServiceTags":["primary"],"ServicePort":5432}]'].join, myself_hostname)
       allow_any_instance_of(ConsulHelper::Helper).to receive(:serch_node_possession_tag).and_return(JSON.parse(response))
     end
 
@@ -33,7 +32,7 @@ describe 'postgresql_part::deploy' do
           }
           chef_run.converge(described_recipe)
         end
-  
+
         describe 'migration url is included applications parameter' do
           it 'download migration file' do
             url = 'http://cloudconductor.org/migration.sql'
@@ -44,7 +43,7 @@ describe 'postgresql_part::deploy' do
             )
           end
         end
-  
+
         describe 'migration type is not included' do
           it 'create migration file' do
             query = 'migration sql strings'
@@ -55,33 +54,33 @@ describe 'postgresql_part::deploy' do
             )
           end
         end
-  
+
         describe 'tables is not exist in postgresql db' do
           it 'do migration' do
             db_host = '127.0.0.1'
             db_port = '5432'
             db_user = 'pgsql'
             db_pass = 'pgpass'
-  
+
             postgresql_connection_info = {
               host: db_host,
               port: db_port,
               username: db_user,
               password: db_pass
             }
-  
+
             chef_run.node.set['postgresql']['config']['port'] = db_port
             chef_run.node.set['postgresql_part']['application']['user'] = db_user
             chef_run.node.set['postgresql_part']['application']['password'] = db_pass
-  
+
             db_name = 'app_db'
             chef_run.node.set['postgresql_part']['application']['database'] = db_name
-  
+
             allow_any_instance_of(Mixlib::ShellOut).to receive(:runcommand)
             allow_any_instance_of(Mixlib::ShellOut).to receive(:stdout).and_return('0')
-  
+
             chef_run.converge(described_recipe)
-  
+
             expect(chef_run).to ChefSpec::Matchers::ResourceMatcher.new(
               :postgresql_database,
               :query,
@@ -91,16 +90,16 @@ describe 'postgresql_part::deploy' do
             )
           end
         end
-  
+
         describe 'tables is exist in postgresql db' do
           it 'do not migration' do
             db_name = 'app_db'
             chef_run.node.set['postgresql_part']['application']['database'] = db_name
-  
+
             allow_any_instance_of(Mixlib::ShellOut).to receive(:runcommand)
             allow_any_instance_of(Mixlib::ShellOut).to receive(:stdout).and_return('1')
             chef_run.converge(described_recipe)
-  
+
             expect(chef_run).to_not ChefSpec::Matchers::ResourceMatcher.new(
               :postgresql_database,
               :query,
@@ -115,28 +114,37 @@ describe 'postgresql_part::deploy' do
     before do
       chef_run.node.set['cloudconductor']['applications'] = {
         app_name => {
-          type: 'dynamic'
+          type: 'dynamic',
+          parameters: {
+            migration: {
+              type: 'sql'
+            }
+          }
         }
       }
+
       chef_run.converge(described_recipe)
+
       response = format(['[{"Node":"%s","Address":"172.17.0.1","ServiceID":"db",',
-                             '"ServiceName":"db","ServiceTags":["primary"],"ServicePort":5432}]'].join, partner_hostname)
+                         '"ServiceName":"db","ServiceTags":["primary"],"ServicePort":5432}]'].join, partner_hostname)
       allow_any_instance_of(ConsulHelper::Helper).to receive(:serch_node_possession_tag).and_return(JSON.parse(response))
     end
+    describe 'tables is not exist in postgresql db' do
+      it 'do not migration' do
+        db_name = 'app_db'
+        chef_run.node.set['postgresql_part']['application']['database'] = db_name
 
-    it 'do not migration' do
-      db_name = 'app_db'
-      chef_run.node.set['postgresql_part']['application']['database'] = db_name
-  
-      allow_any_instance_of(Mixlib::ShellOut).to receive(:runcommand)
-      allow_any_instance_of(Mixlib::ShellOut).to receive(:stdout).and_return('1')
-      chef_run.converge(described_recipe)
-  
-      expect(chef_run).to_not ChefSpec::Matchers::ResourceMatcher.new(
-        :postgresql_database,
-        :query,
-        db_name
-      )
+        allow_any_instance_of(Mixlib::ShellOut).to receive(:runcommand)
+        allow_any_instance_of(Mixlib::ShellOut).to receive(:stdout).and_return('0')
+
+        chef_run.converge(described_recipe)
+
+        expect(chef_run).to_not ChefSpec::Matchers::ResourceMatcher.new(
+          :postgresql_database,
+          :query,
+          db_name
+        )
+      end
     end
   end
 end
