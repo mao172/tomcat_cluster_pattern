234aebc9fff52b688b68ab36f20d630419cef4a0
takasix <maruoka.takashi@tis.co.jp>
2015-01-27 04:08:51 +0000

    Add RPM repository url of PostgreSQL9.4

--- site-cookbooks/postgresql_part/attributes/default.rb
+++ site-cookbooks/postgresql_part/attributes/default.rb
@@ -12,3 +12,18 @@ default['postgresql']['config']['data_directory'] = node['postgresql']['dir']
 default['postgresql_part']['application']['database'] = 'application'
 default['postgresql_part']['application']['user'] = 'application'
 default['postgresql_part']['application']['password'] = 'todo_replace_random_password'
+
+# To merge because does not contain a "pgdg-repo" of postgresql 9.4 to postgresql cookbook now.
+# if contains a "pgdg-repo" to postgresql cookbook to use it
+unless node['postgresql']['pgdg']['repo_rpm_url'].include?("9.4")
+  normal['postgresql']['pgdg']['repo_rpm_url'] = {
+    "9.4" => {
+      "centos" => {
+        "6" => {
+          "i386" => "http://yum.postgresql.org/9.4/redhat/rhel-6-i386/pgdg-centos94-9.4-1.noarch.rpm",
+          "x86_64" => "http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-1.noarch.rpm"
+        }
+      }
+    }
+  }
+end
