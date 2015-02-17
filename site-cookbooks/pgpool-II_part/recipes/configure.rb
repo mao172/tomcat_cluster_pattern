84bff6291df4f75ca158c275ce9f74170a519d25
Takashi Maruoka <maruoka.takashi@tis.co.jp>
2015-02-17 06:44:27 +0000

    Add watch dog settings

--- site-cookbooks/pgpool-II_part/recipes/configure.rb
+++ site-cookbooks/pgpool-II_part/recipes/configure.rb
@@ -1,6 +1,10 @@
 require 'digest/md5'
 
-db_servers = node['cloudconductor']['servers'].select { |_name, server| server['roles'].include?('db') }
+def servers(role)
+  node['cloudconductor']['servers'].select { |_name, server| server['roles'].include?(role) }
+end
+
+db_servers = servers('db')
 
 db_servers.each_with_index do | (_name, server), index |
   node.set['pgpool_part']['pgconf']["backend_hostname#{index}"] = server['private_ip']
@@ -8,6 +12,17 @@ db_servers.each_with_index do | (_name, server), index |
   node.set['pgpool_part']['pgconf']["backend_data_directory#{index}"] = node['pgpool_part']['postgresql']['dir']
   node.set['pgpool_part']['pgconf']["backend_flag#{index}"] = node['pgpool_part']['pgconf']['backend_flag0']
   node.set['pgpool_part']['pgconf']["backend_weight#{index}"] = node['pgpool_part']['pgconf']['backend_weight0']
+end
+
+ap_servers = servers('ap')
+other_ap_servers = ap_servers.delete_if { |_key, val| val['private_ip'] == node[:ipaddress] }
+
+other_ap_servers.each_with_index do | (_name, server), index |
+  node.set['pgpool_part']['pgconf']["other_pgpool_hostname#{index}"] = server['private_ip']
+  node.set['pgpool_part']['pgconf']["other_pgpool_port#{index}"] = node['pgpool_part']['pgconf']['port']
+  node.set['pgpool_part']['pgconf']["other_wd_port#{index}"] = node['pgpool_part']['pgconf']['wd_port']
+  node.set['pgpool_part']['pgconf']["heartbeat_destination#{index}"] = server['private_ip']
+  node.set['pgpool_part']['pgconf']["heartbeat_destination_port#{index}"] = node['pgpool_part']['pgconf']['wd_heartbeat_port']
 end
 
 file "#{node['pgpool_part']['config']['dir']}/pcp.conf" do
--- site-cookbooks/pgpool-II_part/spec/recipes/configure_spec.rb
+++ site-cookbooks/pgpool-II_part/spec/recipes/configure_spec.rb
@@ -10,9 +10,16 @@ describe 'pgpool-II_part::configure' do
     chef_run.node.set['pgpool_part']['config']['dir'] = pgpool_dir
     chef_run.node.set['pgpool_part']['postgresql']['dir'] = '/var/lib/pgsql/9.4/data'
     chef_run.node.set['pgpool_part']['postgresql']['port'] = '5432'
+    chef_run.node.set['pgpool_part']['pgconf']['port'] = 9999
     chef_run.node.set['pgpool_part']['pgconf']['backend_flag0'] = 'ALLOW_TO_FAILOVER'
     chef_run.node.set['pgpool_part']['pgconf']['backend_weight0'] = 1
-    chef_run.node.set['cloudconductor']['servers'] = { 'foo' => { 'roles' => 'db', 'private_ip' => '127.0.0.1' } }
+    chef_run.node.set['pgpool_part']['pgconf']['wd_port'] = 9000
+    chef_run.node.set['pgpool_part']['pgconf']['wd_heartbeat_port'] = 9694
+    chef_run.node.set['cloudconductor']['servers'] = {
+      'db1' => { 'roles' => 'db', 'private_ip' => '127.0.0.1' },
+      'ap1' => { 'roles' => 'ap', 'private_ip' => '127.0.0.101' }
+    }
+    chef_run.node.automatic_attrs['ipaddress'] = '127.0.0.101'
     chef_run.converge(described_recipe)
   end
 
@@ -61,11 +68,13 @@ describe 'pgpool-II_part::configure' do
     end
 
     it 'backend_flag1 or later is the same value as the backend_flag0' do
-      expect(chef_run.node['pgpool_part']['pgconf']['backend_flag1']).to eq(chef_run.node['pgpool_part']['pgconf']['backend_flag0'])
+      expect(chef_run.node['pgpool_part']['pgconf']['backend_flag1'])
+        .to eq(chef_run.node['pgpool_part']['pgconf']['backend_flag0'])
     end
 
     it 'backend_weight1 or later is the same value as the backend_weight0' do
-      expect(chef_run.node['pgpool_part']['pgconf']['backend_weight1']).to eq(chef_run.node['pgpool_part']['pgconf']['backend_weight0'])
+      expect(chef_run.node['pgpool_part']['pgconf']['backend_weight1'])
+        .to eq(chef_run.node['pgpool_part']['pgconf']['backend_weight0'])
     end
 
     it 'write to pgpool.conf all of the nodes as a backend' do
@@ -83,6 +92,117 @@ describe 'pgpool-II_part::configure' do
         .with_content(/#{"backend_data_directory1".ljust(25)} = '\/var\/lib\/pgsql\/9.4\/data'/)
       expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf")
         .with_content(/#{"backend_flag1".ljust(25)} = 'ALLOW_TO_FAILOVER'/)
+    end
+  end
+
+  describe 'node of ap role is own node only' do
+    it 'other_pgpool_hostname settings attribute is not create' do
+      expect(chef_run.node['pgpool_part']['pgconf'].include?('other_pgpool_hostname')).to be_falsey
+    end
+    it 'other_pgpool_port settings attribute is not create' do
+      expect(chef_run.node['pgpool_part']['pgconf'].include?('other_pgpool_port')).to be_falsey
+    end
+    it 'other_wd_port settings attribute is not create' do
+      expect(chef_run.node['pgpool_part']['pgconf'].include?('other_wd_port')).to be_falsey
+    end
+  end
+
+  describe 'only one node of ap role other than its own node' do
+    before do
+      chef_run.node.set['cloudconductor']['servers'] = {
+        'db1' => { 'roles' => 'db', 'private_ip' => '127.0.0.1' },
+        'ap1' => { 'roles' => 'ap', 'private_ip' => '127.0.0.101' },
+        'ap2' => { 'roles' => 'ap', 'private_ip' => '127.0.0.102' }
+      }
+      chef_run.converge(described_recipe)
+    end
+
+    it 'other_pgpool_hostname settings is one made and value is not own private_ip of ap role node' do
+      expect(chef_run.node['pgpool_part']['pgconf']['other_pgpool_hostname0']).to eq('127.0.0.102')
+    end
+
+    it 'other_pgpool_port settings is one made and  value is equal port value' do
+      expect(chef_run.node['pgpool_part']['pgconf']['other_pgpool_port0']).to eq(chef_run.node['pgpool_part']['pgconf']['port'])
+    end
+
+    it 'other_wd_port settings is one made and value is equal wd_port value' do
+      expect(chef_run.node['pgpool_part']['pgconf']['other_wd_port0']).to eq(chef_run.node['pgpool_part']['pgconf']['wd_port'])
+    end
+
+    it 'heartbeat_destination settings is one made and value is not own private_ip of ap role node' do
+      expect(chef_run.node['pgpool_part']['pgconf']['heartbeat_destination0']).to eq('127.0.0.102')
+    end
+
+    it 'heartbeat_destination_port settings is one made and value is wd_heartbeat_port value' do
+      expect(chef_run.node['pgpool_part']['pgconf']['heartbeat_destination_port0'])
+        .to eq(chef_run.node['pgpool_part']['pgconf']['wd_heartbeat_port'])
+    end
+    it 'each settings is write to pgpool.conf' do
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf")
+        .with_content(/#{"other_pgpool_hostname0".ljust(25)} = '127.0.0.102'/)
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf").with_content(/#{"other_pgpool_port0".ljust(25)} = 9999/)
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf").with_content(/#{"other_wd_port0".ljust(25)} = 9000/)
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf")
+        .with_content(/#{"heartbeat_destination0".ljust(25)} = '127.0.0.102'/)
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf")
+        .with_content(/#{"heartbeat_destination_port0".ljust(25)} = 9694/)
+    end
+  end
+  describe 'multiple node of ap role other than its own node' do
+    before do
+      chef_run.node.set['cloudconductor']['servers'] = {
+        'db1' => { 'roles' => 'db', 'private_ip' => '127.0.0.1' },
+        'ap1' => { 'roles' => 'ap', 'private_ip' => '127.0.0.101' },
+        'ap2' => { 'roles' => 'ap', 'private_ip' => '127.0.0.102' },
+        'ap3' => { 'roles' => 'ap', 'private_ip' => '127.0.0.103' }
+      }
+      chef_run.converge(described_recipe)
+    end
+
+    it 'other_pgpool_hostname settings is made the number of other ap nodes and value is private_ip of thatnode' do
+      expect(chef_run.node['pgpool_part']['pgconf']['other_pgpool_hostname0']).to eq('127.0.0.102')
+      expect(chef_run.node['pgpool_part']['pgconf']['other_pgpool_hostname1']).to eq('127.0.0.103')
+    end
+
+    it 'other_pgpool_port settings is made the number of other ap nodes and value is equal port value' do
+      expect(chef_run.node['pgpool_part']['pgconf']['other_pgpool_port0']).to eq(chef_run.node['pgpool_part']['pgconf']['port'])
+      expect(chef_run.node['pgpool_part']['pgconf']['other_pgpool_port1']).to eq(chef_run.node['pgpool_part']['pgconf']['port'])
+    end
+
+    it 'other_wd_port settings is made the number of other ap nodes and value is equal wd_port value' do
+      expect(chef_run.node['pgpool_part']['pgconf']['other_wd_port0']).to eq(chef_run.node['pgpool_part']['pgconf']['wd_port'])
+      expect(chef_run.node['pgpool_part']['pgconf']['other_wd_port1']).to eq(chef_run.node['pgpool_part']['pgconf']['wd_port'])
+    end
+
+    it 'heartbeat_destination settings is made the number of other ap nodes and value is private_ip of that node' do
+      expect(chef_run.node['pgpool_part']['pgconf']['heartbeat_destination0']).to eq('127.0.0.102')
+      expect(chef_run.node['pgpool_part']['pgconf']['heartbeat_destination1']).to eq('127.0.0.103')
+    end
+
+    it 'heartbeat_destination_port settings is made the number of other ap nodes and value is wd_heartbeat_port value' do
+      expect(chef_run.node['pgpool_part']['pgconf']['heartbeat_destination_port0'])
+        .to eq(chef_run.node['pgpool_part']['pgconf']['wd_heartbeat_port'])
+      expect(chef_run.node['pgpool_part']['pgconf']['heartbeat_destination_port1'])
+        .to eq(chef_run.node['pgpool_part']['pgconf']['wd_heartbeat_port'])
+    end
+    it 'each settings is write to pgpool.conf' do
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf")
+        .with_content(/#{"other_pgpool_hostname0".ljust(25)} = '127.0.0.102'/)
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf").with_content(/#{"other_pgpool_port0".ljust(25)} = 9999/)
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf").with_content(/#{"other_wd_port0".ljust(25)} = 9000/)
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf")
+        .with_content(/#{"heartbeat_destination0".ljust(25)} = '127.0.0.102'/)
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf")
+        .with_content(/#{"heartbeat_destination_port0".ljust(25)} = 9694/)
+
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf")
+        .with_content(/#{"other_pgpool_hostname1".ljust(25)} = '127.0.0.103'/)
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf").with_content(/#{"other_pgpool_port1".ljust(25)} = 9999/)
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf").with_content(/#{"other_wd_port1".ljust(25)} = 9000/)
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf")
+        .with_content(/#{"heartbeat_destination1".ljust(25)} = '127.0.0.103'/)
+      expect(chef_run).to render_file("#{pgpool_dir}/pgpool.conf")
+        .with_content(/#{"heartbeat_destination_port1".ljust(25)} = 9694/)
     end
   end
 end
