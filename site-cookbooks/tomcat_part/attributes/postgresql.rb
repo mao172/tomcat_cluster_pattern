82c69bd4488a42a1ac0eeed99a1f9d095207f9eb
Masafumi Okamoto <okamoto.masafumi@tis.co.jp>
2015-02-27 07:04:15 +0000

    Add attributes for postgresql client installation into tomcat_part cookbook in order to matched postgresql version on postgresql_part cookbook

---
+++ site-cookbooks/tomcat_part/attributes/postgresql.rb
@@ -0,0 +1,27 @@
+# this attributes for instration of PostgreSQL-Client
+#
+default['postgresql']['enable_pgdg_yum'] = true
+default['postgresql']['version'] = '9.4'
+default['postgresql']['client']['packages'] = ["postgresql#{node['postgresql']['version'].split('.').join}-devel"]
+default['postgresql']['server']['packages'] = ["postgresql#{node['postgresql']['version'].split('.').join}-server"]
+default['postgresql']['contrib']['packages'] = ["postgresql#{node['postgresql']['version'].split('.').join}-contrib"]
+default['postgresql']['dir'] = "/var/lib/pgsql/#{node['postgresql']['version']}/data"
+# default['postgresql']['server']['service_name'] = "postgresql-#{node['postgresql']['version']}"
+# default['postgresql']['password']['postgres'] = 'todo_replace_random_password'
+# default['postgresql']['config']['listen_addresses'] = '*'
+# default['postgresql']['config']['data_directory'] = node['postgresql']['dir']
+
+# To merge because does not contain a "pgdg-repo" of postgresql 9.4 to postgresql cookbook now.
+# # if contains a "pgdg-repo" to postgresql cookbook to use it
+unless node['postgresql']['pgdg']['repo_rpm_url'].include?('9.4')
+  normal['postgresql']['pgdg']['repo_rpm_url'] = {
+    '9.4' => {
+      'centos' => {
+        '6' => {
+          'i386' => 'http://yum.postgresql.org/9.4/redhat/rhel-6-i386/pgdg-centos94-9.4-1.noarch.rpm',
+          'x86_64' => 'http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-1.noarch.rpm'
+        }
+      }
+    }
+  }
+end
