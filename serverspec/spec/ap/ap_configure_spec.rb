bf2533e006b391d9e47473945676976473d2cb41
Masafumi Okamoto <okamoto.masafumi@tis.co.jp>
2015-03-17 01:20:02 +0000

    Fix test case of using hping3 in serverspec

--- serverspec/spec/ap/ap_configure_spec.rb
+++ serverspec/spec/ap/ap_configure_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
+require 'socket'
+
+def private_ip()
+  Socket.getifaddrs.select{|x|
+    x.name == "eth0" and x.addr.ipv4?
+  }.first.addr.ip_address
+end
 
 def servers(role)
   property[:servers].each_value.select { |server| server[:roles].include?(role) }
@@ -33,16 +40,27 @@ describe port(pgpool_port) do
   it { should be_listening.with('tcp') }
 end
 
-describe port(wd_port) do
-  it { should be_listening.with('tcp') }
+if servers('ap').length > 1
+  describe port(wd_port) do
+    it { should be_listening.with('tcp') }
+  end
 end
 
-servers('ap').each do |server|
-  describe command("hping3 -S #{server[:private_ip]} -p #{pgpool_port} -c 5") do
-    its(:stdout) { should match(/sport=#{pgpool_port} flags=SA/) }
-  end
-  describe command("hping3 -S #{server[:private_ip]} -p #{wd_port} -c 5") do
-    its(:stdout) { should match(/sport=#{wd_port} flags=SA/) }
+describe command("hping3 -S localhost -p #{pgpool_port} -c 5") do
+  its(:stdout) { should match(/sport=#{pgpool_port} flags=SA/) }
+end
+
+if servers('ap').length > 1
+  servers('ap').each do |server|
+    if server[:private_ip] == private_ip
+      describe command("hping3 -S localhost -p #{wd_port} -c 5") do
+        its(:stdout) { should match(/sport=#{wd_port} flags=SA/) }
+      end
+    else
+      describe command("hping3 -S #{server[:private_ip]} -p #{wd_port} -c 5") do
+        its(:stdout) { should match(/sport=#{wd_port} flags=SA/) }
+      end
+    end
   end
 end
 
--- serverspec/spec/web/web_configure_spec.rb
+++ serverspec/spec/web/web_configure_spec.rb
@@ -12,14 +12,6 @@ describe port(80) do
 end
 
 # Check connect ap servers
-describe 'connect ap_servers' do
-  ap_servers = property[:servers].each_value.select do |server|
-    server[:roles].include?('ap')
-  end
-
-  ap_servers.each do |server|
-    describe command("hping3 -S #{server[:private_ip]} -p 8009 -c 5") do
-      its(:stdout) { should match(/sport=8009 flags=SA/) }
-    end
-  end
+describe command("hping3 -S localhost -p 8009 -c 5") do
+  its(:stdout) { should match(/sport=8009 flags=SA/) }
 end
