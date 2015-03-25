475a9921b24fee72253efe3d317cb75a1169aa3e
Masafumi Okamoto <okamoto.masafumi@tis.co.jp>
2015-03-25 06:10:51 +0000

    Fix to be check of has not been specified in the argument

--- site-cookbooks/pgpool-II_part/files/default/failover.sh
+++ site-cookbooks/pgpool-II_part/files/default/failover.sh
@@ -1,6 +1,17 @@
-#!/bin/bash -x
+#!/bin/sh
+
+if [ "$1" == "" ] ; then
+  echo "new master node has not been specified in the argument !!"
+  exit -1
+fi
 
 NEW_MASTER_NODE=`consul members | grep ${1} | awk '{print $1}'`
 
-consul event -name="failover" -node="$NEW_MASTER_NODE"
+echo "NEW_MASTER_NODE=${NEW_MASTER_NODE}"
 
+if [ "$NEW_MASTER_NODE" == ""  ] ; then
+  echo "new master node name is not found !!"
+  exit -1
+fi
+
+consul event -name="failover" -node="$NEW_MASTER_NODE"
